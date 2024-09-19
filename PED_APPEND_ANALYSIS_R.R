125afe261ccb51abd5c6584cb5c5f248d12672e9
Joe LaVigne <jmlavigne510@gmail.com>
2024-09-19 15:02:35 -0400

    shape of data revealed. WBC distribution visualized.

--- PED_APPEND_ANALYSIS_R.R
+++ PED_APPEND_ANALYSIS_R.R
@@ -10,3 +10,21 @@ View(append)
 
 wbc <- append%>%select(WBC_Count)
 View(wbc)
+
+#testing quantitatively for a linear relationship by computing the correlation coefficient. 
+
+coeff_1 <- cor.test(append$WBC_Count, append$Age)
+coeff_1$estimate
+
+#this demonstrates that there is a very low correlation, and therefore a weak linear relationship between these two variables.
+
+coeff_2 <- cor.test(append$WBC_Count, append$Neutrophil_Percentage)
+coeff_2$estimate
+
+#the value 0.6634112 demonstrates a greater linear linear relationship than is demonstrated in coeff_1. 
+
+str(append)
+
+
+WBC_dist <- append%>%ggplot(aes(WBC_Count)) + geom_bar()
+WBC_dist
