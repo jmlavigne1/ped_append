089f1833f26d6d1cb7d9dff8d550bad66db3ed9d
Joe LaVigne <jmlavigne510@gmail.com>
2024-09-19 12:28:44 -0400

    indicator for merged = True

--- ped_append_analysis_ML.py
+++ ped_append_analysis_ML.py
@@ -67,8 +67,8 @@ sn_2 = ped_append.dropna(subset=['WBC_Count'])
 print(sn_2.info)
 #take the subset of sn_1 and sn_2 to make a new dataframe. This will yield a 54 observation long dataframe.
 
-sn_3 = pd.merge(sn_1, sn_2, on = "Segmented_Neutrophils", how = "left")
-
+sn_3 = pd.merge(sn_1, sn_2, on = "Segmented_Neutrophils", indicator=True)
+print(sn_3)
 sn_3 = sn_3.drop_duplicates()
 print(sn_3.info)
 
